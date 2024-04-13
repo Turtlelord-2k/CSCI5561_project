d44a4f6f61cfe8fdc29758d3368a274800e6645c
Santo Santhosh <santo.mat.santhosh@gmail.com>
2024-04-12 21:06:03 -0500

    conda env and basic struct

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import os
+import cv2
+import tensorflow as tf
+
+
+downsample_rates = [8, 16, 32]
+
+def stem_module(input_tensor):
+    pass
+
+def cell_module():
+    pass
+
+def head_module():
+    pass
+
+def FasterSeg():
+    pass
+
+def zoomed_conv():
+    pass
+
+def searchable_superkernel():
+    pass
+
